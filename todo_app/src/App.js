63df5dc0274747cc057cfe794e2b12a1e89c3c36
Mohit Rane <mrrane99@gmail.com>
2020-07-10 03:00:42 +0530

    Defined Component for rendering Todo titles

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import Todos from './components/Todos'
 import './App.css';
 
 class App extends Component {
- render(){
+  state = {
+    todos:[
+      {
+        id:1,
+        title:'Take out the trash',
+        completed: false
+      },
+      {
+        id:2,
+        title:'Dinner with friends',
+        completed: false
+      },
+      {
+        id:1,
+        title:'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  render(){
     return (
       <div className="App">
-        <div>Hello</div>
+        <Todos todos={this.state.todos} />
       </div>
     );
   };
