5f6c400ad7967f2b1d083d7145bb9c94656f0e45
Mohit Rane <mrrane99@gmail.com>
2020-07-11 23:38:24 +0530

    Added About Page with Router Cntd

---
+++ todo_app/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList app v0.1.0. It is part of our new services.</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
