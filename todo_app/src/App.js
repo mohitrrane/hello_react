cf274cdc65b471b5c73c90b926337c578ed7489b
Mohit Rane <mrrane99@gmail.com>
2020-07-11 23:36:38 +0530

    Added About Page with Router

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import {v4 as uuid} from 'uuid';
 import Todos from './components/Todos';
-import Header from './components/layout/header';
+import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About'
 import './App.css';
 
 class App extends Component {
@@ -52,17 +54,24 @@ class App extends Component {
   }
   render(){
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos 
-            key={this.state.todos.id} 
-            todos={this.state.todos} 
-            markComplete={this.markComplete}
-            delTodo={this.delTodo}/>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos 
+                  key={this.state.todos.id} 
+                  todos={this.state.todos} 
+                  markComplete={this.markComplete}
+                  delTodo={this.delTodo}/>
+              </React.Fragment>
+            )} />
+            <Route exact path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   };
 } 
--- todo_app/src/components/layout/header.js
+++ todo_app/src/components/layout/header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={linkStyle} to="/">Home</Link> |
+            <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -15,4 +18,9 @@ const headerStyle = {
     padding: '10px'
 }
 
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none',
+    padding: '3px'
+}
 export default Header;
