f662bde07a78b8cd42ad5046b2a88685053d841b
Mohit Rane <mrrane99@gmail.com>
2020-07-12 17:01:40 +0530

    Viewing Posts

--- redux_posts/src/components/Post.js
+++ redux_posts/src/components/Post.js
@@ -15,9 +15,16 @@ class Post extends Component {
     }
 
     render() {
+        const postItems = this.state.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            </div>
+        ))
         return (
             <div>
                 <h1>Posts</h1>
+                { postItems }
             </div>
         )
     }
